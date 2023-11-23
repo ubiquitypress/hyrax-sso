61f55319a2240931fd7a9e73bc2a3e1aaafe4c1d
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-23 18:46:01 +0000

    REPO-2041 - Folder structure changes

--- lib/hyrax/sso/engine.rb
+++ lib/hyrax/sso/engine.rb
@@ -4,11 +4,6 @@
 module Hyrax
   module Sso
     class Engine < ::Rails::Engine
-      config.to_prepare do
-        Dir.glob(Rails.root + "app/views/hyrax/sso/**/*").each do |c|
-          Rails.application.config.assets.precompile += [c] unless File.directory?(c)
-        end
-      end
     end
   end
 end
