b1e11f52e8665ee8f8b34a61a97b91aa9a5b54ba
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-24 01:41:41 +0000

    REPO-2041 - Rolling back the previous change and adding an artificial delay to ensure that each timestamp is unique.

--- lib/generators/hyrax/sso/install_generator.rb
+++ lib/generators/hyrax/sso/install_generator.rb
@@ -11,13 +11,16 @@ module Hyrax
 
         # Implement a next migration number in line with the usual Rails convention
         def self.next_migration_number(path)
-          Time.now.utc.strftime("%Y%m%d%H%M%S%L")
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
         end
 
         def copy_migrations
           migration_template '20230301152739_add_sso_attributes_to_accounts.rb', 'db/migrate/add_sso_attributes_to_accounts.rb'
+          sleep 0.1 # pauses execution for 0.1 seconds
           migration_template '20230317071542_add_workos_connection_id_to_accounts.rb', 'db/migrate/add_workos_connection_id_to_accounts.rb'
+          sleep 0.1 # pauses execution for 0.1 seconds
           migration_template '20230320114316_clean_up_work_os_fields.rb', 'db/migrate/clean_up_work_os_fields.rb'
+          sleep 0.1 # pauses execution for 0.1 seconds
           migration_template '20230411011527_add_work_os_managed_domain_to_accounts.rb', 'db/migrate/add_work_os_managed_domain_to_accounts.rb'
         end
       end
