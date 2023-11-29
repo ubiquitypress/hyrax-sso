1bbd0c708cfdf9e7fb47c3cba251ef324cc267bb
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-29 09:30:35 +0000

    REPO-2041 - Attempting to load proprietor/accounts/edit from SSO gem

--- lib/generators/hyrax/sso/install_generator.rb
+++ lib/generators/hyrax/sso/install_generator.rb
@@ -17,6 +17,26 @@ module Hyrax
         def copy_migrations
           migration_template '20231127152739_add_fields_for_sso.rb', 'db/migrate/add_fields_for_sso.rb'
         end
+
+        def add_to_proprietor_account
+          path = %w[app views proprietor accounts edit.html.erb]
+
+          file_path_to_add_edit_account = File.join(Rails.root.to_s, *path)
+
+          # if Object.const_defined?(:HykuAddons)
+          #   file_path_to_add_content = File.join(::HykuAddons::Engine.root.to_s, *path)
+          # elsif Object.const_defined?(:Hyku)
+          #   file_path_to_add_content = File.join(Rails.root.to_s, *path)
+          # elsif Object.const_defined?(:Hyrax) && !Object.const_defined?(:Hyku) && !Object.const_defined?(:HykuAddons)
+          #   file_path_to_add_content = File.join(::Hyrax::Engine.root.to_s, *path)
+          # end
+
+          append_to_file file_path_to_add_edit_account do
+            <<-"RUBY"
+            <%= render "hyrax/sso/proprietor/accounts/edit" %>
+          RUBY
+          end
+        end
       end
     end
   end
