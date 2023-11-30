ca5f80919758a9e2a77836b03d1b71f854a268bb
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-30 17:28:57 +0000

    REPO-2041 - Override the views from your main application in your Ruby gem

--- lib/hyrax/sso/engine.rb
+++ lib/hyrax/sso/engine.rb
@@ -6,17 +6,9 @@ module Hyrax
     class Engine < ::Rails::Engine
       isolate_namespace Hyrax::Sso
 
-      # If you have any generators
-      config.generators do |g|
-        g.test_framework :rspec
-      end
-
       initializer 'hyrax-sso.prepend_view_paths', before: :load_config_initializers do |app|
         app.config.paths['app/views'].unshift(root.join('app/views'))
       end
-
-      # To load views from the gem instead of the application
-      config.autoload_paths += %W(#{config.root}/app)
     end
   end
 end
