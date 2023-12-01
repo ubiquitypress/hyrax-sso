c10cb8bdc8baf2ced35e68ed71e21b55c8e7da9d
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-12-01 10:37:54 +0000

    REPO-2041 - Testing what HykuKnapsack did to resolve the ordering issue.

--- lib/hyrax/sso/engine.rb
+++ lib/hyrax/sso/engine.rb
@@ -6,8 +6,14 @@ module Hyrax
     class Engine < ::Rails::Engine
       isolate_namespace Hyrax::Sso
 
-      initializer 'hyrax-sso.prepend_view_paths', before: :load_config_initializers do |app|
-        app.config.paths['app/views'].unshift(root.join('app/views'))
+      config.after_initialize do
+        my_engine_root = Hyrax::Sso::Engine.root.to_s
+        paths = ActionController::Base.view_paths.collect{|p| p.to_s}
+        # This is the opposite of what you usually want to do. Normally app views override engine views
+        # but in our case things in the Hyrax-sso override what is in the application
+        paths = [my_engine_root + '/app/views'] + paths
+        ActionController::Base.view_paths = paths.uniq
+        ::ApplicationController.send :helper, Hyrax::Sso::Engine.helpers
       end
     end
   end
