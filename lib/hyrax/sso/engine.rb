36d5827ff564e29c50184b190683b0b783e8744c
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-12-01 00:11:03 +0000

    REPO-2041 - Debugging the path for custom views

--- lib/hyrax/sso/engine.rb
+++ lib/hyrax/sso/engine.rb
@@ -6,6 +6,9 @@ module Hyrax
     class Engine < ::Rails::Engine
       isolate_namespace Hyrax::Sso
 
+      sso_view_path = Hyrax::Sso::Engine.root.join('app/views')
+      puts "LOG_sso_view_path" + sso_view_path
+
       initializer 'hyrax-sso.prepend_view_paths', before: :load_config_initializers do |app|
         app.config.paths['app/views'].unshift(root.join('app/views'))
       end
