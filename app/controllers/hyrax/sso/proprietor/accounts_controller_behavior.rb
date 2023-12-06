e43493bdaf36278e4029c575a79a961dc91fa949
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-12-06 00:09:01 +0000

    REPO-2041 - Overriding controller - test

--- app/controllers/hyrax/sso/proprietor/accounts_controller_behavior.rb
+++ app/controllers/hyrax/sso/proprietor/accounts_controller_behavior.rb
@@ -5,26 +5,30 @@ module Hyrax
       module AccountsControllerBehavior
         extend ActiveSupport::Concern
 
-        private
+        included do
+          Rails.logger.debug "Hyrax::Sso::Proprietor::AccountsControllerBehavior has been included"
 
-        def edit_account_params
-          params.require(:account).permit(:name,
-                                          :cname,
-                                          :title,
-                                          :is_public,
-                                          :search_only,
-                                          :work_os_organisation,
-                                          :work_os_managed_domain,
-                                          :enable_sso,
-                                          *@account.live_settings.keys,
-                                          admin_emails: [],
-                                          full_account_cross_searches_attributes: [:id,
-                                                                                   :_destroy,
-                                                                                   :full_account_id,
-                                                                                   full_account_attributes: [:id]],
-                                          solr_endpoint_attributes: %i[id url],
-                                          fcrepo_endpoint_attributes: %i[id url base_path],
-                                          data_cite_endpoint_attributes: %i[mode prefix username password])
+          private
+
+          def edit_account_params
+            params.require(:account).permit(:name,
+                                            :cname,
+                                            :title,
+                                            :is_public,
+                                            :search_only,
+                                            :work_os_organisation,
+                                            :work_os_managed_domain,
+                                            :enable_sso,
+                                            *@account.live_settings.keys,
+                                            admin_emails: [],
+                                            full_account_cross_searches_attributes: [:id,
+                                                                                     :_destroy,
+                                                                                     :full_account_id,
+                                                                                     full_account_attributes: [:id]],
+                                            solr_endpoint_attributes: %i[id url],
+                                            fcrepo_endpoint_attributes: %i[id url base_path],
+                                            data_cite_endpoint_attributes: %i[mode prefix username password])
+          end
         end
       end
     end
