06bc5ab757c7be4bf7538ce1548ec5bea007d2ef
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-12-06 00:49:39 +0000

    REPO-2041 - Overriding controller - code refinement

--- app/controllers/hyrax/sso/proprietor/accounts_controller_behavior.rb
+++ app/controllers/hyrax/sso/proprietor/accounts_controller_behavior.rb
@@ -9,14 +9,24 @@ module Hyrax
 
           private
 
-          def original_account_params
-            params.require(:account).permit(super)
-          end
-
           def edit_account_params
-            params.require(:account).permit(:work_os_organisation,
+            params.require(:account).permit(:name,
+                                            :cname,
+                                            :title,
+                                            :is_public,
+                                            :search_only,
+                                            :work_os_organisation,
                                             :work_os_managed_domain,
-                                            :enable_sso).merge(original_account_params)
+                                            :enable_sso,
+                                            *@account.live_settings.keys,
+                                            admin_emails: [],
+                                            full_account_cross_searches_attributes: [:id,
+                                                                                     :_destroy,
+                                                                                     :full_account_id,
+                                                                                     full_account_attributes: [:id]],
+                                            solr_endpoint_attributes: %i[id url],
+                                            fcrepo_endpoint_attributes: %i[id url base_path],
+                                            data_cite_endpoint_attributes: %i[mode prefix username password])
           end
         end
       end
