fc5d068ad1d125708dde1aec2ab886f9118c6c5a
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-12-07 17:22:39 +0000

    REPO-2041 - getting the tests to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "hyrax/sso"
+require "hyrax/sso/engine"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
--- spec/sso/sso_auth_service_spec.rb
+++ spec/sso/sso_auth_service_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe Hyrax::Sso do
       service = Hyrax::Sso::AuthService.new(host: "up.repo")
       expect(service.generate_authorisation_url).to start_with "https://api.workos.com/sso/authorize?client_id"
     end
+
+    # consider adding negative test cases
+    it "raises error when host is omitted" do
+      expect { Hyrax::Sso::AuthService.new }.to raise_error(ArgumentError)
+    end
   end
 end
